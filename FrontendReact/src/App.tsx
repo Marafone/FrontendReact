e1e063e7c42570598fbc495cff59af73a71bffe2
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-12-01 22:04:21 +0100

    basic routing structure created, bootstrap imported

--- FrontendReact/src/App.tsx
+++ FrontendReact/src/App.tsx
@@ -1,8 +1,28 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import MainLayout from "./layouts/MainLayout";
+
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </>
+    )
+  );
+
   return (
     <div>
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- FrontendReact/src/main.tsx
+++ FrontendReact/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import "bootstrap/dist/css/bootstrap.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./styles/Navbar.css";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
