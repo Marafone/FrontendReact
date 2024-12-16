e9dc8d38913d9b2ccf03d51aadf0693f99f76d39
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-12-16 21:16:26 +0100

    add GameCreation as one of route elements

--- FrontendReact/src/App.tsx
+++ FrontendReact/src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
+import GameCreation from "./pages/GameCreation";
 
 function App() {
   const router = createBrowserRouter(
@@ -13,6 +14,7 @@ function App() {
       <>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+          <Route path="create-game" element={<GameCreation/>} />
         </Route>
       </>
     )
