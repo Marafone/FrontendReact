4aacdf838550aed3826f8030560aad4c6a1ebefb
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-12-17 18:44:49 +0100

    game creation css added

--- FrontendReact/src/main.tsx
+++ FrontendReact/src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./styles/Navbar.css";
+import "./styles/navbar.css";
+import "./styles/game-creation-form.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
