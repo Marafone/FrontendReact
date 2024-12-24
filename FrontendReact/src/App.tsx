937cfda2c3e6a142575e7cd2f33ba54d469c90db
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-12-24 15:47:38 +0100

    routing for GameWaitingRoom added

--- FrontendReact/src/App.tsx
+++ FrontendReact/src/App.tsx
@@ -7,15 +7,19 @@ import {
 import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
 import GameCreation from "./pages/GameCreation";
+import GameWaitingRoom from "./pages/GameWaitingRoom";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
+        {/* Routes using MainLayout */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="create-game" element={<GameCreation/>} />
+          <Route path="create-game" element={<GameCreation />} />
         </Route>
+        {/* Routes without MainLayout */}
+        <Route path="/wait-for-game" element={<GameWaitingRoom />}></Route>
       </>
     )
   );
