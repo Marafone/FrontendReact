cb6749f01c1018ec1857d8cf848d4e38b6023d5f
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-12-29 22:33:35 +0100

    websocket event types added

---
+++ FrontendReact/src/events/game-playing-room/WebSocketEventTypes.tsx
@@ -0,0 +1,3 @@
+interface BaseEvent {
+  eventType: string;
+}
---
+++ FrontendReact/src/events/game-waiting-room/WebSocketEventTypes.tsx
@@ -0,0 +1,20 @@
+interface BaseEvent {
+  eventType: string;
+}
+
+export type Team = "RED" | "BLUE";
+
+export interface TeamStateEvent extends BaseEvent {
+  eventType: "TeamState";
+  teamState: Map<string, Team>;
+}
+
+export interface PlayerJoinedEvent extends BaseEvent {
+  eventType: "PlayerJoinedEvent";
+  playerName: string;
+  team: Team;
+}
+
+export interface GameStartedEvent extends BaseEvent {
+  eventType: "GameStartedEvent";
+}
