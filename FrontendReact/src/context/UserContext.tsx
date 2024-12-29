41424e49c16b0386b685c406a5ceaf2fe6327c06
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-12-29 22:35:50 +0100

    user context added to store username globally (need to be changed because of localStorage)

--- FrontendReact/src/context/UserContext.tsx
+++ FrontendReact/src/context/UserContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 interface UserData {
   username: string | null;
@@ -7,8 +7,17 @@ interface UserData {
 
 export const UserContext = createContext<UserData | undefined>(undefined);
 
-export const UserProvider: React.FC<any> = ({children}) => {
-  const [username, setUsername] = useState<string | null>(null);
+export const UserProvider: React.FC<any> = ({ children }) => {
+  const [username, setUsername] = useState<string | null>(() => {
+    const storedUsername = localStorage.getItem("usernameValue");
+    return storedUsername ? JSON.parse(storedUsername) : null;
+  });
+
+  // save username to localStorage every time username is changed
+  useEffect(() => {
+    localStorage.setItem("usernameValue", JSON.stringify(username)); // TODO don't store it in localStorage, use cookie instead
+  }, [username]);
+
   return (
     <UserContext.Provider value={{ username, setUsername }}>
       {children}
@@ -19,7 +28,9 @@ export const UserProvider: React.FC<any> = ({children}) => {
 export const useUserContext = () => {
   const userContext = useContext(UserContext);
   if (userContext === undefined) {
-    throw new Error("You should use UserProvider instead of UserContext.Provider");
+    throw new Error(
+      "You should use UserProvider instead of UserContext.Provider"
+    );
   }
   return userContext;
 };
