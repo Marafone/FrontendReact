8b8298436df59a5349d8ec9e18e94e2a61327289
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-12-30 22:42:33 +0100

    new events added

--- FrontendReact/src/events/game-playing-room/WebSocketEventTypes.tsx
+++ FrontendReact/src/events/game-playing-room/WebSocketEventTypes.tsx
@@ -1,3 +1,34 @@
 interface BaseEvent {
   eventType: string;
 }
+
+export type Card = {
+  id: bigint;
+  rank: string;
+  suit: string;
+};
+
+export interface TurnState extends BaseEvent {
+  eventType: "TurnState";
+  turn: Map<string, Card>; // username -> card
+}
+
+export interface MyCardsState extends BaseEvent {
+  eventType: "MyCardsState";
+  myCards: Card[];
+}
+
+export interface PointState extends BaseEvent {
+  eventType: "PointState";
+  playerPointState: Map<string, number>; // username -> points
+}
+
+export interface PlayersOrderState extends BaseEvent {
+  eventType: "PlayersOrderState";
+  playersOrder: string[];
+}
+
+export interface TrumpSuitState extends BaseEvent {
+  eventType: "TrumpSuitState";
+  trumpSuit: string;
+}
