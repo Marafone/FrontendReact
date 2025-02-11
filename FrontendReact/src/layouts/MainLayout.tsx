36880101a7f86813ead39c8d5f5df0a2d1529177
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-02-11 13:58:33 +0100

    option to center elements in outlet added, height adjusted to fit entire screen

--- FrontendReact/src/layouts/MainLayout.tsx
+++ FrontendReact/src/layouts/MainLayout.tsx
@@ -1,23 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import { useEffect} from "react";
+import { useEffect } from "react";
 import "../styles/main-layout.css";
 import { useTheme } from "../context/ThemeContext";
 
 const MainLayout = () => {
-
   const { theme } = useTheme();
-  
+  const location = useLocation();
+
   useEffect(() => {
-      document.documentElement.setAttribute("data-theme", theme);
-    }, [theme]);
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const centeredRoutes = ["/players-ranking", "/create-game"];
+  const isCentered = centeredRoutes.includes(location.pathname);
 
   return (
     <div>
       <Navbar />
       <div
-        className="main-layout"
-        style={{ height: "92vh"}}
+        className={`${
+          isCentered ? "d-flex justify-content-center align-items-center" : ""
+        } main-layout`}
       >
         <Outlet />
       </div>
