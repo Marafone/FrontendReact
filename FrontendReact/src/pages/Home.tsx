6350d541a8190ea196d286f77e201da7820a0abd
Blizni13 <szymongryz@interia.pl>
2025-04-29 21:41:48 +0200

    MAR-41 Code refactored, games marked as private and non-private, user can join private games using password

--- FrontendReact/src/pages/Home.tsx
+++ FrontendReact/src/pages/Home.tsx
@@ -1,230 +1,249 @@
 import axiosWithLogout from "../axios";
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { LanguageContext } from "../context/LanguageContext";
-import { useTheme } from "../context/ThemeContext";
+import {useContext, useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {LanguageContext} from "../context/LanguageContext";
+import {useTheme} from "../context/ThemeContext";
 import "../styles/home-page.css";
-import { useUserContext } from "../context/UserContext";
-
-enum GameType {
-  Maraffa = "Maraffa",
-  Briscolla = "Briscolla",
-  Trisette = "Trisette",
-}
-
-interface GameData {
-  gameId: bigint;
-  gameName: string;
-  gameType: GameType;
-  joinedPlayersAmount: number;
-}
+import {useUserContext} from "../context/UserContext";
+import {toast} from "react-toastify";
+import JoinGameModal from "../components/JoinGameModal.tsx";
+import GameData from "../types/game.ts";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const playersAmount = 4;
-  const [page, setPage] = useState(1);
-  const [lobbies, setLobbies] = useState<GameData[]>([]);
-  const [loading, setLoading] = useState(true);
-  // reconnection option
-  const [isReconnectionPossible, setIsReconnectionPossible] = useState(false);
-  const [reconnectableGameId, setReconnectableGameId] = useState<bigint>(
-    BigInt(0)
-  );
+    const navigate = useNavigate();
+    const playersAmount = 4;
+    const [page, setPage] = useState(1);
+    const [lobbies, setLobbies] = useState<GameData[]>([]);
+    const [loading, setLoading] = useState(true);
+    // reconnection option
+    const [isReconnectionPossible, setIsReconnectionPossible] = useState(false);
+    const [reconnectableGameId, setReconnectableGameId] = useState<bigint>(
+        BigInt(0)
+    );
 
-  const context = useContext(LanguageContext);
-  const { username, setUsername } = useUserContext();
+    const [selectedGame, setSelectedGame] = useState<GameData | null>(null);
 
-  const { theme } = useTheme();
+    const context = useContext(LanguageContext);
+    const {username, setUsername} = useUserContext();
 
-  if (!context) {
-    throw new Error("LanguageContext must be used within a LanguageProvider.");
-  }
+    const {theme} = useTheme();
 
-  const { translate } = context; // Now `context` is guaranteed to be defined
+    if (!context) {
+        throw new Error("LanguageContext must be used within a LanguageProvider.");
+    }
 
-  useEffect(() => {
-      if (username == null) // only send request to get active games when user is logged in
-        return
-    
-      axios // use standard axios to not redirect to login on 401 http status code received
-      .get("/game/active", {
-        transformResponse: [(data) => data], // disable automatic parsing
-      })
-      .then((response) => {
-        if (response.data) {
-          setIsReconnectionPossible(true);
-          setReconnectableGameId(response.data);
-        }
-      }).catch((error) => {
-        if(error.response.status == 401){
-          setUsername(null)
-        }
-        console.log(error)
-      });
-  }, []);
+    const {translate} = context; // Now `context` is guaranteed to be defined
 
-  useEffect(() => {
-    const fetchGames = () => {
-      axiosWithLogout.get("/game/waiting").then((response) => {
-        setLobbies(response.data);
-      });
-    };
-    setLoading(true);
-    fetchGames()
-    setLoading(false);
+    useEffect(() => {
+        if (username == null) // only send request to get active games when user is logged in
+            return
 
-    //keep refreshing games
-    const intervalID = setInterval(fetchGames, 4000);
+        axios // use standard axios to not redirect to login on 401 http status code received
+            .get("/game/active", {
+                transformResponse: [(data) => data], // disable automatic parsing
+            })
+            .then((response) => {
+                if (response.data) {
+                    setIsReconnectionPossible(true);
+                    setReconnectableGameId(response.data);
+                }
+            }).catch((error) => {
+            if (error.response.status == 401) {
+                setUsername(null)
+            }
+            console.log(error)
+        });
+    }, []);
 
-    return () => clearInterval(intervalID)
-  }, []);
+    useEffect(() => {
+        const fetchGames = () => {
+            axiosWithLogout.get("/game/waiting").then((response) => {
+                setLobbies(response.data);
+            });
+        };
+        setLoading(true);
+        fetchGames()
+        setLoading(false);
 
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
+        //keep refreshing games
+        const intervalID = setInterval(fetchGames, 4000);
 
-  const handleNavigation = (gameData: GameData) => {
-    navigate("/wait-for-game", { state: gameData });
-  };
+        return () => clearInterval(intervalID)
+    }, []);
 
-  const handleJoinGame = (
-    gameData: GameData,
-    team: string,
-    joinGameCode: string
-  ) => {
-    const joinGameRequestData = {
-      team: team,
-      joinGameCode: joinGameCode,
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+    }, [theme]);
+
+    const handleNavigation = (gameData: GameData) => {
+        navigate("/wait-for-game", {state: gameData});
     };
 
-    axiosWithLogout
-      .post(`/game/${gameData.gameId}/join`, joinGameRequestData)
-      .then(() => {
-        handleNavigation(gameData);
-      });
-  };
+    const handleJoinGame = (
+        gameData: GameData,
+        joinGameCode: string
+    ) => {
+        const joinGameRequestData = {
+            joinGameCode: joinGameCode,
+        };
 
-  const handleReconnect = (gameId: bigint) => {
-    setIsReconnectionPossible(false);
-    navigate("/play-game", {
-      state: { gameId: BigInt(gameId) },
-    });
-  };
+        axiosWithLogout
+            .post(`/game/${gameData.gameId}/join`, joinGameRequestData)
+            .then(() => {
+                handleNavigation(gameData);
+            })
+            .catch((error) => {
+                toast.error(error.response.data.message);
+            });
+    };
 
-  return (
-    <>
-      <div className="container-fluid d-flex flex-column align-items-center h-100 p-3 w-100">
-        {isReconnectionPossible && (
-          <div className="w-50 mb-1 mt-2">
-            <button
-              className="btn btn-success w-100 fw-bold"
-              onClick={() => handleReconnect(reconnectableGameId)}
-            >
-              Reconnect
-            </button>
-          </div>
-        )}
-        {/* Create Game Button */}
-        <div className="w-50 mb-4 mt-2">
-          <Link
-            to="/create-game"
-            className="custom-create-game-btn btn btn-primary border border-black border-opacity-25 w-100 w-md-auto fw-bold"
-          >
-            {translate("home.createGameBtn")} {/* Create Game Button */}
-          </Link>
-        </div>
+    const handleLobbyClick = (gameData: GameData) => {
+        if (gameData.isPrivate)
+            setSelectedGame(gameData);
+        else
+            handleJoinGame(gameData, "");
+    }
 
-        <div className="row w-100">
-          {/* Left Column: News and Create Game Button */}
-          <div className="col-12 col-md-4 mb-3">
-            {/* News Section */}
-            <div>
-              <h1>{translate("home.newsTitle")}</h1> {/* News Title */}
-              <p>{translate("home.newsContent")}</p> {/* News Content */}
-            </div>
-          </div>
+    const handleReconnect = (gameId: bigint) => {
+        setIsReconnectionPossible(false);
+        navigate("/play-game", {
+            state: {gameId: BigInt(gameId)},
+        });
+    };
 
-          {/* Right Column: Lobby Table */}
-          <div className="col-12 col-md-8 mb-3">
-            <div className="custom-lobby-div d-flex flex-column w-100 border border-black border-opacity-25 border-2">
-              {/* Header */}
-              <div className="d-flex justify-content-between align-items-center container-fluid border-bottom border-black border-opacity-25 border-1 p-2">
-                <p className="fw-bold m-0">{translate("home.lobbyName")}</p>{" "}
-                {/* Lobby Name */}
-                <p className="fw-bold m-0">{translate("home.gameType")}</p>{" "}
-                {/* Game Type */}
-                <p className="fw-bold m-0">{translate("home.players")}</p>{" "}
-                {/* Players */}
-              </div>
-              {/* Lobbies Info */}
-              {loading ? (
-                <div className="d-flex justify-content-center align-items-center container-fluid border-bottom border-black border-opacity-25 border-1 p-2">
-                  <p className="m-0">{translate("home.loading")}</p>{" "}
-                  {/* Loading */}
+    function renderLobbies() {
+        if (loading) {
+            return (
+                <div
+                    className="d-flex justify-content-center align-items-center container-fluid border-bottom border-black border-opacity-25 border-1 p-2">
+                    <p className="m-0">{translate("home.loading")}</p>{" "}
                 </div>
-              ) : lobbies.length == 0 ? (
+            );
+        }
+
+        if (lobbies.length === 0) {
+            return (
                 <div className="border-bottom border-black border-opacity-25 border-1 p-2">
-                  No games available
+                    No games available
                 </div>
-              ) : (
-                lobbies.map((l) => (
-                  <div
-                    key={l.gameId}
-                    className="custom-lobby-div-element d-flex justify-content-between align-items-center container-fluid border-bottom border-black border-opacity-25 border-1 p-2"
-                    onClick={() => {
-                      const gameData: GameData = {
-                        gameId: l.gameId,
-                        gameName: l.gameName,
-                        gameType: l.gameType,
-                        joinedPlayersAmount: l.joinedPlayersAmount,
-                      };
-                      handleJoinGame(gameData, "RED", "");
-                    }}
-                  >
-                    <p className="m-0">{l.gameName}</p>
-                    <p className="m-0">{l.gameType}</p>
-                    <p className="m-0">
-                      {l.joinedPlayersAmount}/{playersAmount}
-                    </p>
-                  </div>
-                ))
-              )}
-              {/* Pagination */}
-              <div className="d-flex justify-content-between align-items-center container-fluid p-2 mt-auto border-black border-top border-opacity-25">
-                <div
-                  className="d-flex align-items-center gap-2 fw-bold"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => {
-                    page > 1 && setPage(page - 1);
-                  }}
-                >
-                  <i className="bi bi-arrow-left"></i>
-                  <p className="m-0">{translate("home.prevPage")}</p>{" "}
-                  {/* Prev */}
-                </div>
-                <p className="fw-bold m-0">
-                  {translate("home.pageText")} {page}
-                </p>{" "}
-                {/* Page Text */}
-                <div
-                  className="d-flex align-items-center gap-2 fw-bold"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => {
-                    setPage(page + 1);
-                  }}
-                >
-                  <p className="m-0">{translate("home.nextPage")}</p>{" "}
-                  {/* Next */}
-                  <i className="bi bi-arrow-right"></i>
-                </div>
-              </div>
+            );
+        }
+
+        return lobbies.map((gameData) => renderLobby(gameData));
+    }
+
+    function renderLobby(gameData: GameData) {
+        return (
+            <div
+                key={gameData.gameId}
+                className="custom-lobby-div-element row border-bottom border-black border-opacity-25 border-1 p-2"
+                onClick={() => handleLobbyClick(gameData)}>
+                <p className="m-0 col-4 text-start">
+                    {gameData.isPrivate && <i className="bi bi-lock-fill m-0 p-0"></i>}
+                    {gameData.gameName}
+                </p>
+                <p className="m-0 col-4 text-center">{gameData.gameType}</p>
+                <p className="m-0 col-4 text-end">
+                    {gameData.joinedPlayersAmount}/{playersAmount}
+                </p>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        )
+    }
+
+    return (
+        <>
+            {selectedGame &&
+                <JoinGameModal onHide={() => setSelectedGame(null)} onJoin={handleJoinGame} gameData={selectedGame}/>}
+            <div className="container-fluid d-flex flex-column align-items-center h-100 p-3 w-100">
+                {isReconnectionPossible && (
+                    <div className="w-50 mb-1 mt-2">
+                        <button
+                            className="btn btn-success w-100 fw-bold"
+                            onClick={() => handleReconnect(reconnectableGameId)}
+                        >
+                            Reconnect
+                        </button>
+                    </div>
+                )}
+                {/* Create Game Button */}
+                <div className="w-50 mb-4 mt-2">
+                    <Link
+                        to="/create-game"
+                        className="custom-create-game-btn btn btn-primary border border-black border-opacity-25 w-100 w-md-auto fw-bold"
+                    >
+                        {translate("home.createGameBtn")} {/* Create Game Button */}
+                    </Link>
+                </div>
+
+                <div className="row w-100">
+                    {/* Left Column: News and Create Game Button */}
+                    <div className="col-12 col-md-4 mb-3">
+                        {/* News Section */}
+                        <div>
+                            <h1>{translate("home.newsTitle")}</h1> {/* News Title */}
+                            <p>{translate("home.newsContent")}</p> {/* News Content */}
+                        </div>
+                    </div>
+
+                    {/* Right Column: Lobby Table */}
+                    <div className="col-12 col-md-8 mb-3">
+                        <div
+                            className="custom-lobby-div d-flex flex-column w-100 border border-black border-opacity-25 border-2">
+                            {/* Header */}
+                            <div
+                                className="row border-bottom border-black border-opacity-25 border-1 p-2">
+                                <p className="fw-bold m-0 col-4 text-left">{translate("home.lobbyName")}</p>{" "}
+                                {/* Lobby Name */}
+                                <p className="fw-bold m-0 col-4 text-center">{translate("home.gameType")}</p>{" "}
+                                {/* Game Type */}
+                                <p className="fw-bold m-0 col-4 text-end">{translate("home.players")}</p>{" "}
+                                {/* Players */}
+                            </div>
+                            {/* Lobbies Info */}
+                            {renderLobbies()}
+                            {/* Pagination */}
+                            <div
+                                className="row p-2 mt-auto border-black border-top border-opacity-25">
+                                {/* Prev */}
+                                <div className="col-5 d-flex justify-content-end">
+                                    <button
+                                        disabled={page <= 1}
+                                        className="custom-lobby-change-page-btn fw-bold border-0"
+                                        onClick={() => {
+                                            if (page > 1)
+                                                setPage(page - 1);
+                                        }}
+                                    >
+                                        <i className="bi bi-arrow-left me-1"></i>
+                                        <span className="m-0">{translate("home.prevPage")}</span>{" "}
+                                    </button>
+                                </div>
+
+                                {/* Page Text */}
+                                <p className="col-2 fw-bold m-0 text-center">
+                                    {translate("home.pageText")} {page}
+                                </p>{" "}
+
+                                {/* Next */}
+                                <div className="col-5 d-flex justify-content-start">
+                                    <button
+                                        className="custom-lobby-change-page-btn gap-2 fw-bold border-0 text-start"
+                                        onClick={() => {
+                                            setPage(page + 1);
+                                        }}
+                                    >
+                                        <span className="m-0">{translate("home.nextPage")}</span>{" "}
+                                        <i className="bi bi-arrow-right ms-1"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
